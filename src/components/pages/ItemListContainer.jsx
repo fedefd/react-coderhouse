d46900abad399b93343961ae805cbb1bc87e052d
fedefd <fedem_04@hotmail.com>
2024-02-11 19:13:02 -0300

    se creo el ItemList presentacional

--- src/components/pages/ItemListContainer.jsx
+++ src/components/pages/ItemListContainer.jsx
@@ -1,15 +1,8 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
-const ItemListContainer = ({ greeting }) => {
+import ItemList from './ItemList';
+const ItemListContainer = ({greeting}) => {
   return (
-    <Box sx={{ textAlign: 'center', mt: 4 }}>
-      <Typography variant="h4" component="h2" gutterBottom>
-        {greeting}
-      </Typography>
-      {/* Aquí puedes añadir más contenido */}
-    </Box>
+    <ItemList greeting={greeting} />
   );
 };
 
