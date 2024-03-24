02a9b5ffa8ff4fcb0cc6773646304d27a5832671
fedefd <fedem_04@hotmail.com>
2024-03-24 00:22:36 -0300

    se estilizo y se acomodo el footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,48 @@
+import { Box, Grid, Paper, Typography, IconButton } from "@mui/material";
+
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import "./Footer.css";
 const Footer = () => {
+  const footerWidth = window.innerWidth > 600 ? "1080px" : "85%";
+  const handleIconClick = (url) => {
+    window.open(url, "_blank");
+  };
   return (
     <>
-      <div>footer</div>
+      <Box className="footer-container">
+        <Paper style={{ maxWidth: footerWidth }} className="footer-paper">
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={1}
+          >
+            <Grid item xs={2}>
+              <Typography variant="body2" className="footer-text">
+                © {new Date().getFullYear()} Bookpolis
+              </Typography>
+            </Grid>
+            <Grid item xs={9} className="footer-icons">
+              {[
+                { icon: <TwitterIcon />, url: "https://www.twitter.com" },
+                { icon: <InstagramIcon />, url: "https://www.instagram.com" },
+                { icon: <LinkedInIcon />, url: "https://www.linkedin.com" },
+              ].map((item, index) => (
+                <IconButton
+                  key={index}
+                  color="#fff"
+                  onClick={() => handleIconClick(item.url)}
+                >
+                  {item.icon}
+                </IconButton>
+              ))}
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
     </>
   );
 };
